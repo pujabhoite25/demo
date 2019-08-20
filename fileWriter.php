76e75473725fb3ff3d8991a1f33b4b7ab2c86539
pujabhoite25 <52904184+pujabhoite25@users.noreply.github.com>
2019-08-20 13:46:56 +0530

    Create php file

---
+++ fileWriter.php
@@ -0,0 +1,6 @@
+if (!isset($_POST['params']) && !empty($_POST['params'])) {
+        $params = $_POST['params'];
+
+        $jsonObject = json_encode($params);
+        file_put_contents('rbdata.json', $jsonObject);
+    }
